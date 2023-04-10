3d4ab194820ea78ff4550ee272d8bdda0d6f16ff
Agaba Claire Linda <agabaclaire70@gmail.com>
2023-04-10 19:32:04 +0300

    backend

---
+++ backend/signup/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+    """
+    class SignUp
+        title str
+        firstName str
+        lastName str
+        password 
+        date_created datetime
+    
+    """
