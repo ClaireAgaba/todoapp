1c2b311a6d77497b01f707745ff6ddd7f149190c
Agaba Claire Linda <agabaclaire70@gmail.com>
2023-04-10 19:48:21 +0300

    setting up the backend

--- backend/signup/models.py
+++ backend/signup/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-    """
-    class SignUp
-        title str
-        firstName str
-        lastName str
-        password 
-        date_created datetime
+"""
+class SignUp
+    firstName str
+    lastName str
+    password 
+    date_created datetime
     
-    """
+"""
+class SignUp(models.Model):
+     firstName=models.CharField(max_length=50)
+     lastName=models.CharField(max_length=50)
+     username=models.CharField(max_length=10)
+     password=models.CharField(max_length=130) 
+     
+     def __str__(self):
+          return self.username  
+
